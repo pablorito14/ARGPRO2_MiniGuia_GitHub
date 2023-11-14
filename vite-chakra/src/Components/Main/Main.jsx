fd9a4ff9051aee6554bdf61b27e860a5c796a4fa
Pablo Rito <pablorito14@gmail.com>
2023-11-14 18:10:02 -0300

    pruebas animaciones

--- vite-chakra/src/Components/Main/Main.jsx
+++ vite-chakra/src/Components/Main/Main.jsx
@@ -9,10 +9,10 @@ import pasosReact from '../../p-react.json';
 import pasosVite from '../../p-vite.json';
 import { Paso } from '../Paso/Paso';
 
-import {isValidMotionProp, motion} from 'framer-motion';
+import {motion} from 'framer-motion';
 
-const MotionButton = ({tab,delay,handler}) => {
-  
+const MotionButton = ({tab,delay,handler,hide}) => {
+  // estilo seleccionado: bg='gray.500' color='white'
   return(
     <Box w='full'>
       <motion.div 
@@ -20,11 +20,11 @@ const MotionButton = ({tab,delay,handler}) => {
           transition={{duration: 0.1,delay: delay}} 
           initial={{opacity:0}}>
         <motion.div 
-          whileHover={{ scale: 0.9 }}
-          whileTap={{ scale: 1.1 }}
+          whileHover={{ scale: 0.9}}
+          whileTap={{scale: 1.1 }}
           transition={{ type: "spring", stiffness: 400, damping: 17 }}
         >
-          <Button w='100%' onClick={handler}>{ tab }</Button>
+          <Button w='100%' onClick={handler} isDisabled={hide} >{ tab }</Button>
         </motion.div>  
       </motion.div>
     </Box>
@@ -32,13 +32,13 @@ const MotionButton = ({tab,delay,handler}) => {
 }
 const MotionPasos = ({arrPasos}) => {
   return (
-    <motion.div
-      animate={{opacity:1}} 
-      transition={{duration:1}} 
-      initial={{ opacity:0 }}
-    >
-      {arrPasos.map(paso => <Paso key={paso.id} paso={paso}/>)}
-    </motion.div>
+      <motion.div
+        animate={{opacity:1}} 
+        transition={{duration:0.5}} 
+        initial={{ opacity:0 }}
+      >
+        {arrPasos.map(paso => <Paso key={paso.id} paso={paso}/>)}
+      </motion.div>
   )
 }
 const Main = () => {
@@ -85,33 +85,39 @@ const Main = () => {
   return(
     <Box bg='gray.600'>
       <Container maxW='4xl' py='1rem' >
-
         <Box display='grid' gridTemplateColumns={{base:'repeat(2,1fr)',md:'repeat(4,1fr)'}} gap={2} justifyContent='space-between'>
-          {tabs.map((tab,i) => <MotionButton key={i} tab={tab.label} delay={0.3+(i/5)} handler={tab.handler}/>)}
+          {/* {tabs.map((tab,i) => <MotionButton key={i} tab={tab.label} delay={0.3+(i/5)} handler={tab.handler} show={showBasicos} />)} */}
+          <MotionButton key={1} tab={'Pasos básicos'} delay={0.3+(0/5)} handler={handleShowBasicos} hide={showBasicos} />
+          <MotionButton key={2} tab={'Deploy HTML+CSS+JS'} delay={0.3+(1/5)} handler={handleShowHtmlCss} hide={showHtmlCss} />
+          <MotionButton key={3} tab={'Deploy React'} delay={0.3+(2/5)} handler={handleShowReact} hide={showReact} />
+          <MotionButton key={4} tab={'Deploy Vite'} delay={0.3+(3/5)} handler={handleShowVite} hide={showVite} />
         </Box>
+        {/* <Box display='grid' gridTemplateColumns={{base:'repeat(2,1fr)',md:'repeat(4,1fr)'}} gap={2} justifyContent='space-between'>
+          {tabs.map((tab,i) => <MotionButton key={i} tab={tab.label} delay={0.3+(i/5)} handler={tab.handler} show={showBasicos} />)}
+        </Box> */}
         <motion.div 
             animate={{opacity:1}} 
             transition={{duration: 0.1,delay: 0.3+(tabs.length/5)}} 
             initial={{opacity:0}}>
-          <Divider h='10px' />
+          {/* <Divider h='10px' /> */}
           {showBasicos && 
           <>
-            <Heading color='gray.300' textAlign='center'>{tabs[0].label}</Heading>
+            {/* <Heading color='gray.300' textAlign='center'>{tabs[0].label}</Heading> */}
             <MotionPasos arrPasos={pasosBasicos} /> 
           </>}
           {showHtmlCss && 
           <>
-            <Heading color='gray.300' textAlign='center'>{tabs[1].label}</Heading>
+            {/* <Heading color='gray.300' textAlign='center'>{tabs[1].label}</Heading> */}
             <MotionPasos arrPasos={pasosHtmlCss} /> 
           </>}
           {showReact && 
           <>
-            <Heading color='gray.300' textAlign='center'>{tabs[2].label}</Heading>
+            {/* <Heading color='gray.300' textAlign='center'>{tabs[2].label}</Heading> */}
             <MotionPasos arrPasos={pasosReact} /> 
           </>}
           {showVite && 
           <>
-            <Heading color='gray.300' textAlign='center'>{tabs[3].label}</Heading>
+            {/* <Heading color='gray.300' textAlign='center'>{tabs[3].label}</Heading> */}
             <MotionPasos arrPasos={pasosVite} /> 
           </>}
         </motion.div>
