28e0d7760e9b44825197e48cc0772689c67fbe83
Pablo Rito <pablorito14@gmail.com>
2023-11-14 14:57:34 -0300

    link a repo de git

--- vite-chakra/src/Components/Header/Header.jsx
+++ vite-chakra/src/Components/Header/Header.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Header.css';
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Link, Container, Heading } from '@chakra-ui/react';
+import { FaGithub } from "react-icons/fa6";
+
+import {motion} from 'framer-motion';
+
 
 const Header = () => {
   return(
     <Box as='header' bg='whitesmoke'>
-      <Container  maxW='4xl' >
+      <Container  maxW='4xl' display='flex' justifyContent='space-between' alignItems='center' >
         <Heading>
           Mini-Guia GitHub
         </Heading>
+        <Heading>
+        <motion.div 
+          whileHover={{ scale: 0.9 }}
+          whileTap={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 400, damping: 17 }}
+        >
+
+          <Link href='https://github.com/pablorito14/ARGPRO2_MiniGuia_GitHub/tree/main-vite/vite-chakra' isExternal>
+            <FaGithub />
+          </Link>
+        </motion.div>
+        </Heading>
+
       </Container>
     </Box>
   )
